1c54664c186e55fd84054e1041e804293737bb70
Thomas Nelson <tanelso2@gmail.com>
2017-10-18 21:02:04 -0700

    Fixed the solution for scoring cribbage hands

--- dp-335/src/dp_335/core.clj
+++ dp-335/src/dp_335/core.clj
@@ -1,6 +1,10 @@
 (ns dp-335.core
   (:gen-class)
-  (:require [clojure.math.combinatorics :as combo]))
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.string :as str])
+  (:import (java.lang Integer)))
+
+(defn parse-int [x] (Integer/parseInt x))
 
 (defn- get-value-from-card
   [card]
@@ -25,21 +29,21 @@
   (let [points-map {2 2
                     3 6
                     4 12}]
-    (reduce (fn [accum x]
+    (->> hand
+         (group-by :value)
+         (map #(count (nth % 1)))
+         (reduce
+           (fn [accum x]
               (+ accum (get points-map x 0)))
-            0
-            (map
-              #(count (nth % 1))
-              (group-by :value hand)))))
+           0))))
 
 (defn- get-flushes-score
   [hand]
   (let [suits (map :suit hand)]
-    (if (apply = suits)
-      5
-      (if (apply = (drop-last suits))
-          4
-          0))))
+    (cond
+      (apply = suits) 5
+      (apply = (drop-last suits)) 4
+      :else 0)))
 
 (defn- get-nobs-score
   [hand]
@@ -55,34 +59,70 @@
   [hand]
   (let [subsets (combo/subsets hand)]
     (->> subsets
-         (filter #(= 15
-                     (reduce + 0 (map get-value-from-card %))))
+         (map (partial map get-value-from-card))
+         (map (partial reduce + 0))
+         (filter (partial = 15))
          (count)
          (* 2))))
+
+(defn- sequential-run?
+  [cards]
+  (let [sorted-values (sort (map get-order-from-card cards))]
+    (every? identity
+      (map #(= (- %2 1) %1)
+           sorted-values
+           (rest sorted-values)))))
 
 (defn- get-runs-score
   [hand]
   (let [points-map {3 3
                     4 4
-                    5 5}
-        sorted-values (sort (map get-order-from-card hand))]
-    (->> (map #(= (- %2 1) %1)
-           sorted-values
-           (rest sorted-values))
-         (partition-by identity)
-         (filter first)
+                    5 5}]
+    (->> (combo/subsets hand)
+         (filter sequential-run?)
          (map count)
-         (apply max)
-         (+ 1)
-         (#(get points-map % 0)))))
+         (map #(get points-map % 0))
+         (apply +))))
 
 (defn score-hand
   [hand]
   (reduce +
           0
           (map #(% hand) [get-fifteens-score
-                               get-pairs-score
-                               get-flushes-score
-                               get-nobs-score
-                               get-runs-score])))
+                          get-pairs-score
+                          get-flushes-score
+                          get-nobs-score
+                          get-runs-score])))
 
+(defn- parse-suit-str
+  [s]
+  (case s
+    "D" :diamonds
+    "C" :clubs
+    "H" :hearts
+    "S" :spades))
+
+(defn- parse-value-str
+  [s]
+  (case s
+    ("A" "J" "Q" "K") (keyword s)
+    (parse-int s)))
+
+(defn parse-card-str
+  [card-str]
+  (let [trimmed-card-str (str/trim card-str)
+        split-point (- (count trimmed-card-str) 1)
+        [card-value-str card-suit-str] (map
+                                         (partial apply str)
+                                         (split-at split-point trimmed-card-str))]
+    {:suit (parse-suit-str card-suit-str)
+     :value (parse-value-str card-value-str)}))
+
+(defn parse-hand-str
+  [hand-str]
+  (mapv parse-card-str
+        (str/split (str/trim hand-str) #",")))
+
+(defn score-hand-str
+  [hand-str]
+  (score-hand (parse-hand-str hand-str)))
--- dp-335/test/dp_335/core_test.clj
+++ dp-335/test/dp_335/core_test.clj
@@ -34,3 +34,36 @@
           expected 4
           result (score-hand hand)]
       (is (= expected result)))))
+
+(deftest parse-card-str-test
+  (testing "Parsing cards from strings"
+    (let [test-cases [["AC" {:suit :clubs :value :A}]
+                      ["6D" {:suit :diamonds :value 6}]
+                      ["10C" {:suit :clubs :value 10}]
+                      ["9H" {:suit :hearts :value 9}]
+                      ["KS" {:suit :spades :value :K}]]]
+      (doseq [[test-str expected] test-cases]
+        (let [result (parse-card-str test-str)]
+          (is (= expected result)))))))
+
+(deftest score-hand-str-test
+  (testing "Scoring hands from strings"
+    (let [test-cases [["5D,QS,JC,KH,AC"   10]
+                      ["8C,AD,10C,6H,7S"  7]
+                      ["AC,6D,5C,10C,8C"  4]
+                      ["2C,3C,3D,4D,3S"   17]
+                      ["2C,3C,4D,4D,5S"   14]
+                      ["2H,2C,3S,4D,4S"   18]
+                      ["2H,2C,3S,4D,9S"   12]
+                      ["5H,5C,5S,JD,5D"   29]
+                      ["6D,JH,4H,7S,5H"   15]
+                      ["5C,4C,2C,6H,5H"   12]
+                      ["10C,8D,KS,8S,5H"  6]
+                      ["10C,5C,4C,7S,3H"  7]
+                      ["7D,3D,10H,5S,3H"  8]
+                      ["7C,KD,9D,8H,3H"   5]
+                      ["8S,AC,QH,2H,3H"   5]
+                      ["5H,5C,5S,JD,5D"   29]]]
+      (doseq [[test-str expected] test-cases]
+        (let [result (score-hand-str test-str)]
+          (is (= expected result)))))))
